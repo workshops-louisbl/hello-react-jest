b57011f4025309f0edca2e0dbb43600ad4415733
louisbl <louis@beltramo.me>
2020-04-30 11:36:30 +0200

    test: card preview buttons

--- src/components/CardPreview.test.js
+++ src/components/CardPreview.test.js
@@ -1,33 +1,75 @@
 import React from 'react';
 import { CardPreview } from "./CardPreview";
-import { render, fireEvent } from "@testing-library/react";
+import { render, fireEvent, act } from "@testing-library/react";
+
+function setup() {
+  const term = "This is a test term";
+  const definition = "This is the definition of a test term";
+  const utils = render(<CardPreview term={term} definition={definition} />);
+
+  return {
+    term,
+    definition,
+    utils
+  }
+}
 
 it("shows term correctly", () => {
-  // arrange
-  const term = "This a test term";
-
-  // act
-  const utils = render(<CardPreview term={term} />);
+  const {term, utils} = setup();
 
   // assert
   expect(utils.getByText(term)).toBeInTheDocument();
 })
 
 it("has a button to show definition", () => {
-  const utils = render(<CardPreview />);
+  const {utils} = setup();
 
   expect(utils.getByText("show back")).toBeVisible()
 })
 
 it("shows definition when it flips the card", () => {
-  const term = "This is a test term";
-  const definition = "This is the definition of a test term";
-
-  const utils = render(<CardPreview term={term} definition={definition} />);
+  const {term, definition, utils} = setup();
 
   const showButton = utils.getByRole("button", {name: "show back"});
   fireEvent.click(showButton);
 
   expect(utils.getByText(definition)).toBeInTheDocument();
   expect(utils.queryByText(term)).not.toBeInTheDocument();
+})
+
+it("flips the card back to the term", () => {
+  const {term, definition, utils} = setup();
+
+  const showBackButton = utils.getByRole("button", {name: "show back"});
+  fireEvent.click(showBackButton);
+
+  const showFrontButton = utils.getByRole("button", {name: "show front"});
+  fireEvent.click(showFrontButton);
+
+  expect(utils.getByText(term)).toBeInTheDocument();
+  expect(utils.queryByText(definition)).not.toBeInTheDocument();
+})
+
+it("can delete the card", () => {
+  window.confirm = jest.fn();
+  const {term, definition, utils} = setup();
+
+  const deleteButton = utils.getByRole("button", {name: "delete"});
+
+  fireEvent.click(deleteButton);
+
+  expect(window.confirm).toBeCalled();
+
+})
+
+it("can edit the card", () => {
+  const {term, definition, utils} = setup();
+
+  const editButton = utils.getByRole("button", {name: "edit"});
+
+  act(() => {
+    fireEvent.click(editButton);
+  })
+
+  expect(editButton).not.toBeInTheDocument();
 })
